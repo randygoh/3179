9e981482c52d0f4d4243ff3d36947897b7795a03
randygoh <115731551+randygoh@users.noreply.github.com>
2022-10-17 19:18:42 +0800

    Final HTML folder

--- asgn2_html/vega_lite_vis.js
+++ asgn2_html/vega_lite_vis.js
@@ -12,3 +12,13 @@ var vg_3 = "us_crime_2016_piechart.vg.json";
 vegaEmbed("#piechart", vg_3).then(function(result) {
     // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error)
+
+var vg_4 = "property_crime_line_chart.vg.json";
+vegaEmbed("#linechart2", vg_4).then(function(result) {
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+
+var vg_5 = "total_crime_area_chart.vg.json";
+vegaEmbed("#areachart", vg_5).then(function(result) {
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
